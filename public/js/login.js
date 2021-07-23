2befa4e92488ee63b7512001c728f843d97008b9
Abdelrahman <abdelrahman.deghedy@gmail.com>
2021-07-23 08:43:19 +0200

    Fix

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ document.querySelector(".login-form").addEventListener("submit", async (e) => {
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
 
-  const url = `http://127.0.0.1:3000/api/v1/user/login`;
+  const url = `https://deghedeeaat.herokuapp.com/login`;
   const data = {
     email,
     password,
